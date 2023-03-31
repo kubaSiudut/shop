839f94ad9586b90883cd073f1e4e261e64c403ce
Kuba Siudut <jak.siudut@gmail.com>
2023-03-31 17:40:58 +0200

    poprawka usówania zdj

--- pages/api/deleteProducts.js
+++ pages/api/deleteProducts.js
@@ -1,5 +1,6 @@
 const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
+const fs = require('fs');
 import * as jose from "jose";
 
 export default async function getOrdersList(req, res) {
@@ -17,7 +18,28 @@ export default async function getOrdersList(req, res) {
  
   if (req.method === "POST" && payload.sub ===1) {
 
-    req.body.map( async (el) =>{  await db.run("DELETE FROM products WHERE id=? ", el);})
+    req.body.map( async (el) =>{ 
+     
+      const name = await (db.get(`SELECT neme FROM products WHERE id= ?`,el))
+      // console.log('plik do usunięcia 1 ')
+      console.log(name)
+      // const nameStr = name.neme 
+      const path =  `./public/uploads/${name.neme }`
+
+      console.log(path)
+      try {
+
+        fs.unlinkSync(path)
+        //file removed
+      } catch(err) {
+
+        console.error(err)
+      }
+      await db.run("DELETE FROM products WHERE id=? ", el);})
+ 
+
+
+
     res.status(200).json({ message : 'OK' })
   }else{
     res.status(400).json({ message : 'NOK' })
